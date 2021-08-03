b7d1b43b081af3ac01ec45c6dab0ce735bb78ada
crazze16 <crazze16@gmail.com>
2021-08-03 14:02:33 +0300

    'chat'

--- src/Chat/chat.tsx
+++ src/Chat/chat.tsx
@@ -1,80 +1,342 @@
 import {
     ButtonGroup,
     ChatBody,
+    ChatSection,
     ChatWrapper,
     ConversationList,
     ConversationListItem,
     Conversations,
-    ConversationsTitle, InputMessage, InputMessageSection, InvitationSubTitle, InvitationTitle,
-    MessageSection, MessageWrapper
+    ConversationsTitle,
+    InputMessage,
+    InputMessageSection,
+    InvitationSubTitle,
+    InvitationTitle,
+    MessageSection,
+    MessageSectionWrapper,
+    MessageWrapper,
+    NewMessageAlert,
+    SelectedConversationTitle,
+    TextInfoContainer
 } from "./styles";
-import {Button, IcoShortArrowRight, IcoThreeDots} from "../components/generic";
+import {Button, IcoShortArrowRight, IcoThreeDots, TitleH1, TitleH2} from "../components/generic";
+import React, {Dispatch, SetStateAction, useCallback, useEffect, useRef, useState} from "react";
+import {NavLink} from "react-router-dom";
+
+type TMyMessage = {
+    value: string
+    isMy: boolean
+}
+
+type TConversation = {
+    name: string
+    userId: number
+    img: string
+}
+
+type TMessageInput = {
+    setMessageData: Dispatch<SetStateAction<Array<TMyMessage>>>
+}
+
+type TInvitationMessage = {
+    isHost: boolean
+}
+
+type TMessage = {
+    value: string
+    isMy: boolean
+}
+
+interface IMessagesList {
+    messageData: Array<TMyMessage>
+    scrollMode: boolean
+    isNewMessages: boolean
+    setScrollMode: (isScrollMode: boolean) => void
+    setIsNewMessages: (isNewMessage: boolean) => void
+}
+
+interface ISingleConversation {
+    activeConversation: TConversation | null
+    setActiveConversation: (conversation: TConversation | null) => void
+    conversation: TConversation
+    setActiveCallback: any
+}
+
+// mock data //
+
+const MockConversationsData: Array<TConversation> = [
+    {name: 'Cor ten Broek', userId: 1, img: 'https://via.placeholder.com/30'},
+    {name: 'Somebody once told me', userId: 12, img: 'https://via.placeholder.com/30'},
+    {name: 'Test name', userId: 3, img: 'https://via.placeholder.com/30'},
+    {name: 'Test name', userId: 4, img: 'https://via.placeholder.com/30'},
+]
 
 export const ChatPage = () => {
+    const [messageData, setMessageData] = useState<Array<TMyMessage>>([]);
+    const [scrollMode, setScrollMode] = useState<boolean>(true);
+    const [isNewMessages, setIsNewMessages] = useState<boolean>(false);
+    const [conversations, setConversations] = useState<Array<TConversation>>([])
+    const [activeConversation, setActiveConversation] = useState<TConversation | null>(null)
+    console.log('ACTIVE CONVERSATION:', activeConversation)
+    // const F = (data: any) => useCallback(() => {
+    //     setActiveConversation(data)
+    // }, [activeConversation])
+
+
+    const setActiveCallback = useCallback((_id) => {
+        console.log(_id);
+        setActiveConversation({name: 'Test name', userId: 3, img: 'https://via.placeholder.com/30'})
+    }, [activeConversation?.userId])
+
+    // const F = useCallback(() => {
+    //     setActiveConversation({name: 'Test name', userId: 3, img: 'https://via.placeholder.com/30'})
+    // }, [activeConversation])
+
+    useEffect(() => {
+        setConversations(MockConversationsData)
+    }, [])
+
     return (
         <ChatWrapper>
-            <ChatBody>
-                <Conversations>
-                    <ConversationsTitle>Active Conversations</ConversationsTitle>
-                    <ConversationList>
-                        <SingleConversation isActive={true}/>
-                        <SingleConversation isActive={false}/>
-                    </ConversationList>
-                </Conversations>
-                <MessageSection>
-                    <Message isMy={true}/>
-                    <Message isMy={false}/>
-                    <HostInvitation/>
-                </MessageSection>
-            </ChatBody>
-            <MessageInput/>
+            <NoChatsInfoSection/>
+            <FirstMeetChatInfoSection/>
+            <GetHelpChatInfoSection/>
+            <SelectedConversationTitle>
+                {
+                    activeConversation && (
+                        <>
+                            <TitleH1>{activeConversation.name}</TitleH1>
+                            <NavLink to={'/'}>View profile</NavLink>
+                        </>
+                    )
+                }
+            </SelectedConversationTitle>
+            <ChatSection>
+                <ChatBody>
+                    <Conversations>
+                        <div>
+                            <ConversationsTitle>Active Conversations</ConversationsTitle>
+                            <ConversationList>
+                                {
+                                    conversations?.map((item, index) =>
+                                        <SingleConversation
+                                            key={index}
+                                            conversation={item}
+                                            activeConversation={activeConversation}
+                                            setActiveConversation={setActiveConversation}
+                                            setActiveCallback={setActiveCallback}
+                                        />
+                                    )
+                                }
+                            </ConversationList>
+                        </div>
+                        <Button outline>Choose this student</Button>
+                    </Conversations>
+                    <MessagesList isNewMessages={isNewMessages} messageData={messageData} scrollMode={scrollMode}
+                                  setScrollMode={setScrollMode} setIsNewMessages={setIsNewMessages}
+                    />
+                </ChatBody>
+                <MessageInput setMessageData={setMessageData}/>
+            </ChatSection>
         </ChatWrapper>
     )
 };
 
 
-const SingleConversation = (props: any) => {
+const MessagesList: React.FC<IMessagesList> = React.memo((props) => {
+    const {messageData, scrollMode, setScrollMode, setIsNewMessages, isNewMessages} = props;
+    console.log('MESSAGE LIST')
+    const messagesAnchorRef = useRef<HTMLDivElement>(null);
 
-    const {isActive} = props;
+    const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const element = e.currentTarget;
+        if ((Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) < 100) && (element.scrollHeight !== element.offsetHeight)) {
+            setScrollMode(true);
+            setIsNewMessages(false);
+        } else {
+            setScrollMode(false);
+        }
+    };
+
+    useEffect(() => {
+        if (scrollMode) {
+            messagesAnchorRef.current?.scrollIntoView({behavior: "smooth"})
+        }
+    }, [messageData, scrollMode]);
+
+    useEffect(() => {
+        if (!scrollMode) {
+            setIsNewMessages(true)
+        }
+    }, [messageData]);
 
     return (
-        <ConversationListItem isActive={isActive}>
-            <img src="https://via.placeholder.com/30" alt=""/>
-            <p>Cor ten Broek</p>
+        <MessageSectionWrapper>
+            {
+                isNewMessages ?
+                    <NewMessageAlert onClick={() => setScrollMode(true)}>New message</NewMessageAlert>
+                    : ''
+            }
+            <MessageSection onScroll={scrollHandler}>
+                <SendMessageInfoSection/>
+                <InvitationMessage isHost={true}/>
+                <InvitationMessage isHost={false}/>
+                {
+                    messageData?.map((item: TMyMessage, index: number) =>
+                        <Message key={index} isMy={item.isMy} value={item.value}/>)
+                }
+                <div ref={messagesAnchorRef}/>
+            </MessageSection>
+        </MessageSectionWrapper>
+    )
+})
+
+const SingleConversation: React.FC<ISingleConversation> = React.memo((props) => {
+    const {activeConversation, setActiveConversation, setActiveCallback} = props;
+    const {name, img, userId} = props.conversation;
+
+    console.log('SINGLE CONV')
+    return (
+        <ConversationListItem isActive={activeConversation?.userId === userId}
+                              onClick={() => setActiveConversation(props.conversation)}>
+            <img src={img} alt="user avatar"/>
+            <p>{name}</p>
             <div><IcoThreeDots size={'1.5em'}/></div>
         </ConversationListItem>
     )
-};
+});
 
-const MessageInput = () => {
+const MessageInput: React.FC<TMessageInput> = (props) => {
+
+    const {setMessageData} = props;
+
+    const [newMessage, setNewMessage] = useState<TMyMessage>({value: '', isMy: true});
+
+    const onInputChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewMessage({...newMessage, value: e.currentTarget.value})
+    }
+
+    const sendMessageHandler = () => {
+        setMessageData((prev: Array<TMyMessage>) => [...prev, newMessage])
+        setNewMessage({...newMessage, value: ''})
+    }
+
     return (
         <InputMessageSection>
-            <InputMessage placeholder='Enter your message here'/>
-            <Button main h={'37px'} fontSize={'16px'} disabled={false}>Send<IcoShortArrowRight size={'2em'}/></Button>
+            <InputMessage placeholder='Enter your message here' onChange={onInputChangeHandler}
+                          value={newMessage?.value}/>
+            <Button main h={'37px'} fontSize={'16px'} disabled={!newMessage?.value}
+                    onClick={sendMessageHandler}>Send<IcoShortArrowRight size={'2em'}/></Button>
         </InputMessageSection>
     )
 };
 
-const HostInvitation = () => {
+const InvitationMessage: React.FC<TInvitationMessage> = (props) => {
+
+    const {isHost} = props;
+
     return (
         <MessageWrapper isMy={false}>
-            <InvitationTitle>Cor ten Broek invites you to come live in this room from 1 may to 30 sept.</InvitationTitle>
-            <InvitationSubTitle>Do you accept the invitation to start the renting process?</InvitationSubTitle>
-            <ButtonGroup>
-                <Button normal h={'37px'} fontWeight={'bold'} fontSize={'16px'} disabled={false}>Decline</Button>
-                <Button main h={'37px'} fontWeight={'bold'}  fontSize={'16px'} disabled={false}>I accept this offer</Button>
-            </ButtonGroup>
+            {
+                isHost ? (
+                    <>
+                        <InvitationTitle>
+                            You have invited Inge Licht to become your tenant from 15 sept - 26 sept.
+                        </InvitationTitle>
+                        <InvitationSubTitle fontWeight={'bold'} color={'sub'}>
+                            Waiting for confirmation...
+                        </InvitationSubTitle>
+                        <ButtonGroup>
+                            <Button normal h={'37px'} fontWeight={'bold'} fontSize={'16px'} disabled={false}>Cancel
+                                Invitation</Button>
+                        </ButtonGroup>
+                    </>
+                ) : (
+                    <>
+                        <InvitationTitle>
+                            Cor ten Broek invites you to come live in this room from 1 may to 30 sept.
+                        </InvitationTitle>
+                        <InvitationSubTitle>
+                            Do you accept the invitation to start the renting process?
+                        </InvitationSubTitle>
+                        <ButtonGroup>
+                            <Button normal h={'37px'} fontWeight={'bold'} fontSize={'16px'}
+                                    disabled={false}>Decline</Button>
+                            <Button main h={'37px'} fontWeight={'bold'} fontSize={'16px'} disabled={false}>I accept this
+                                offer</Button>
+                        </ButtonGroup>
+                    </>
+                )
+            }
         </MessageWrapper>
     )
 };
 
-const Message = (props: any) => {
-
-    const {isMy} = props;
-
+const Message: React.FC<TMessage> = React.memo((props => {
+    const {isMy, value} = props;
     return (
         <MessageWrapper isMy={isMy}>
-            Hi Cor, thanks :)
+            {value.trim()}
         </MessageWrapper>
+    )
+}))
+
+const NoChatsInfoSection = () => {
+    return (
+        <TextInfoContainer width={511}>
+            <TitleH1>My Chats</TitleH1>
+            <div>
+                <p>You don’t have any chats yet. Go to My Rooms to check out new available rooms and find possible
+                    matches!</p>
+            </div>
+            <Button main h={'37px'}>Go To My Rooms</Button>
+        </TextInfoContainer>
+    )
+}
+
+const FirstMeetChatInfoSection = () => {
+    return (
+        <TextInfoContainer>
+            <TitleH1>My Chats</TitleH1>
+            <div>
+                <p>
+                    Here you see an overview of your chats. You can only chat with hosts that have liked your profile.
+                    Use
+                    this chat to get to know each other and discuss the terms of your stay. To increase the chance for a
+                    successful match, we advise you to plan a visit or a video call before you make a decision.
+                </p>
+                <p>
+                    If you and the host reached an agreement, the host can send an invitation. When you accept the
+                    invitation, your stay is confirmed and the adventure can start!
+                </p>
+            </div>
+        </TextInfoContainer>
+    )
+}
+
+const GetHelpChatInfoSection = () => {
+    return (
+        <TextInfoContainer alignCenter border p={'16px 0'}>
+            <TitleH2>Not sure what to do?</TitleH2>
+            <p>
+                Please don’t hesitate to contact Hospi Housing if you have any questions.
+            </p>
+            <p>
+                We are happy to help!
+            </p>
+            <Button normal>Get help from Hospi Housing</Button>
+        </TextInfoContainer>
+    )
+}
+
+const SendMessageInfoSection = () => {
+    return (
+        <TextInfoContainer alignCenter>
+            <p>
+                Send Inge Licht a message to introduce yourself.
+            </p>
+            <p>
+                <span>What would you like to know before meeting this person in your house?</span>
+            </p>
+        </TextInfoContainer>
     )
 }
--- src/Chat/styles.tsx
+++ src/Chat/styles.tsx
@@ -13,28 +13,47 @@ const conversationBackgroundHover = 'rgba(252,244,252,0.03)';
 const selectedConversationColor = '#FFFCF6';
 const dotsReverse = '#6F7F9E';
 const mainText = '#032565'
+const subText = '#6f7f9e'
+const linkColor = '#EA6B26'
 
 export const ChatWrapper = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+export const ChatSection = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 776px;
   width: 100%;
   background: ${chatBackground};
   border: 1px solid ${border};
   border-radius: 4px;
-  padding: 16px;
+  padding: 16px 0 16px 16px;
+  margin-top: 16px;
 `;
 
 export const ChatBody = styled.div`
+  width: 100%;
   display: flex;
   margin-bottom: 16px;
+  position: relative;
 `;
 
 export const Conversations = styled.div`
-  min-width: 206px;
-  border-right: 1px solid ${border};
-  padding-right: 24px;
-  margin-right: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  & > div {
+    width: 210px;
+    border-right: 1px solid ${border};
+    padding-right: 24px;
+    flex: 1;
+    margin-bottom: 16px;
+  }
+  & button {
+    width: 100%;
+    font-weight: bold;
+  }
 `;
 
 export const ConversationsTitle = styled.div`
@@ -46,42 +65,48 @@ export const ConversationsTitle = styled.div`
 
 export const ConversationList = styled.ul`
   padding: 0;
-  margin: 0;
-  list-style-type: none;  
+  margin-top: 8px;
+  list-style-type: none;
+  width: 100%;
 `;
 export const ConversationListItem = styled.li<{ isActive: boolean }>`
   display: flex;
   align-items: center;
-  background: ${props => props.isActive ? `${conversationBackground}` : ''};
+  background: ${props => props.isActive ? conversationBackground : ''};
   width: 100%;
   padding: 8px;
   border-radius: 4px;
+  & :hover {
+    cursor: pointer;
+  }
   & img {
     width: 24px;
     height: 24px;
     border-radius: 50%;
     border: ${props => props.isActive ? '1px solid #FFFFFF' : `1px solid ${border}`}
   }
+
   & p {
-    color: ${selectedConversationColor};
+    color: ${props => props.isActive ? selectedConversationColor : conversationBackground};
     font-size: 16px;
     line-height: 21px;
-    font-weight: 400;
+    font-weight: 600;
     padding-left: 8px;
     flex: 1;
   }
+
   & div {
     & :hover {
-        background: ${conversationBackgroundHover};
-        border-radius: 50%;
-        cursor: pointer;
+      background: ${conversationBackgroundHover};
+      border-radius: 50%;
+      cursor: pointer;
     }
   }
+
   & div svg {
-  color: ${props => props.isActive ? dotsReverse : ''}
+    color: ${props => props.isActive ? dotsReverse : conversationBackground}
   }
 `;
-
 
 
 export const InputMessage = styled.textarea`
@@ -90,55 +115,148 @@ export const InputMessage = styled.textarea`
   border: none;
   font-size: 20px;
   line-height: 26px;
-  padding: 13.5px 16px; 
+  padding: 13.5px 16px;
   resize: none;
 `;
 export const InputMessageSection = styled.div`
-    display: flex;
-    align-items: center;
-    background: #FFFFFF;
-    border-radius: 8px;
-    padding-right: 8px;
-    & button {
+  display: flex;
+  align-items: center;
+  background: #FFFFFF;
+  border-radius: 8px;
+  padding-right: 8px;
+  margin-right: 16px;
+  & button {
     display: flex;
     align-items: center;
     justify-content: center;
-    }
+  }
 `;
 
+export const MessageSectionWrapper = styled.div`
+  position: relative;
+`;
+
+export const SelectedConversationTitle = styled.div`
+  display: flex;
+  align-items: flex-end;
+  min-height: 28px;
+  & h1 {
+    width: fit-content;
+    margin-right: 16px;
+  }
+  & a {
+    color: ${linkColor};
+    text-decoration: none;
+    font-weight: 600;
+  }
+`;
+
+export const NewMessageAlert = styled.div`
+  position: absolute;
+  bottom: 0;
+  background: ${conversationBackground};
+  width: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  border-radius: 6px;
+  color: #FFFFFF;
+  left: calc(50% - 60px);
+  z-index: 10;
+  opacity: 70%;
+  transition: all .2s ease-in-out;
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+    
+  }
+`
+
 export const MessageSection = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  padding: 0 16px 0 24px;
+  height: 500px;
+  overflow-x: auto;
+  position: relative;
 `;
 
 export const MessageWrapper = styled.div<{ isMy: boolean }>`
-    min-height: 42px;
-    background: ${props => props.isMy ? `${conversationBackground}` : `${selectedConversationColor}`};
-    padding: 16px;
-    display: flex;
-    flex-direction: column;
-    font-size: 16px;
-    border-radius: 6px;
-    color: ${props => props.isMy ? `#FFFFFF` : `${mainText}`};
-    line-height: 21px;
-    width:fit-content;
-    align-self: ${props => props.isMy ? 'flex-end' : 'flex-start'};
-    margin-top: 4px;
+  background: ${props => props.isMy ? `${conversationBackground}` : `${selectedConversationColor}`};
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 16px;
+  border-radius: 6px;
+  color: ${props => props.isMy ? `#FFFFFF` : `${mainText}`};
+  line-height: 21px;
+  max-width: 100%;
+  width: fit-content;
+  align-self: ${props => props.isMy ? 'flex-end' : 'flex-start'};
+  margin-top: 4px;
+  word-break: break-all;
+  white-space: pre-wrap;
 `;
 
 export const InvitationTitle = styled.div`
-    text-align: center;
-    font-weight: 600;
+  text-align: center;
+  font-weight: 600;
 `;
-export const InvitationSubTitle = styled.div`
-    text-align: center;
+export const InvitationSubTitle = styled.div<{fontWeight?: string, color?: 'main' | 'sub'}>`
+  text-align: center;
+  font-weight: ${props => props.fontWeight && `${props.fontWeight}`};
+  color: ${props => props.color === 'sub' && `${subText}`} 
 `;
 export const ButtonGroup = styled.div`
-    margin-top: 10px;
-    display: flex;
-    justify-content: center;
-    & button:nth-last-child(1) {
-        margin-left: 10px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  & button:nth-last-child(1) {
+    margin-left: 10px;
+  }
+`;
+
+export const TextInfoContainer = styled.div<{
+    width?: number,
+    alignCenter?: boolean,
+    border?: boolean,
+    p?: string}>`
+  width: 100%;
+  color: ${mainText};
+  margin: 16px 0;
+  ${props => props.alignCenter && `text-align: center`};
+  ${props => props.border && `border: 1px solid ${border}`};
+  ${props => props.border && `border-radius: 4px`};
+  ${props => props.p && `padding: ${props.p}`};
+  & h1 {
+    margin-bottom: 24px;
+  }
+  & h2 {
+    margin-bottom: 8px;
+  }
+  & div {
+    p:not(:last-child) {
+      margin-bottom: 24px;
     }
+  }
+  & p {
+    font-size: 20px;
+    ${props => props.width && `max-width: ${props.width}px`};
+    line-height: 26px;
+
+    & span {
+      color: ${subText};
+    }
+  }
+  & button {
+    margin-top: 24px;
+    font-weight: 600;
+  }
+`;
+export const NoChatsText = styled.p`
+
 `;
